8b976c81aed0855b15c07a041763e96be42a191a
mmaljak <maljak.mario@gmail.com>
2024-01-19 15:22:16 +0100

    Adding the 'Day-17-Trivia'

---
+++ Intermediate/Day-17-Trivia/quiz-game/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
+print(f"You've completed the quiz! \nYour final score was: {quiz_brain.score}/{len(question_bank)}")
